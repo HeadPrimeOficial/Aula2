b14fb6121efd73cfc0377391c7961b3e17b08bc4
HeadPrimeOficial <headtutoriall@gmail.com>
2025-02-21 22:10:16 +0000

    heranca

--- ATIVIDADE8/Main8.java
+++ ATIVIDADE8/Main8.java
@@ -10,7 +10,7 @@ Funcionario funcionario1 = new Funcionario(001, "wesley", "Travessa sao", 981250
 System.out.println(funcionario1.toString());
 */
 //METODO 02
-Funcionario funcionario1= new Funcionario(1032, "Wesley", "Travessa", 98250839, "wesket@gmail",
+Funcionario funcionario1= new Funcionario(1032, "Wesley", "Travessa", 98250839, "wesleyt@gmail",
 new Conta("Santander", 8, 7, "Corrente", 10.000, 10.000));
 
 
