de46f3143d8d4550bc7ab1fbd198bb070fb91e48
HeadPrimeOficial <headtutoriall@gmail.com>
2025-02-27 22:51:20 +0000

    ok

--- Atividade3/Mainativ3.java
+++ Atividade3/Mainativ3.java
@@ -1,7 +1,9 @@
 public class Mainativ3 {
 public static void main(String[] args) {
     
-System.out.println();
+Processador3 processador = new Processador3("Asus", "nhg447");
+
+System.out.println(processador.super.toString));
 
 }
 
--- Atividade3/Processador3.java
+++ Atividade3/Processador3.java
@@ -1,15 +1,22 @@
 public class Processador3 extends Produto {
 
+    public Processador3(String marca, String modelo) {
+        super(marca, modelo);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public String toString() {
+        return "Processador3 "+ super.marcaModelo();
+    }
+
    
    
 
    
     
    
-    @Override
-    public String toString() {
-        return "Processador3 "+super.marcaModelo();
-    }
+   
 
     
 
